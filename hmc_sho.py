36ade38737f51d6bd256e0bc55d1579a96caf41b
Aashish Tripathee <helloworld@aashishtripathee.com>
2017-08-03 22:41:30 -0400

    ABC

--- hmc_sho.py
+++ hmc_sho.py
@@ -8,14 +8,14 @@ N_2 = 1
 N = N_1 + N_2
 k = 1
 delta_T = 0.5
-total_steps = 6
+total_steps = 1000
 
 
 def calculate_force(x_s, gaussian_weight=0.5):
     forces = [0.0] * N
 
     for i in range(N):
-        forces[i] = - ( forces[i] + 0.5 * np.tanh(0.5 * x_s[i]) )
+        forces[i] = - x_s[i]
 
     return forces
 
@@ -30,37 +30,16 @@ def calculate_hamiltonian(x_s, p_s, gaussian_weight=0.5):
         kinetic_contribution += p_s[i] * p_s[i]
     kinetic_contribution *= 0.5
 
-    # Next, compute the action.
-    action = 0.
+    potential_contribution = 0.
     for i in range(N):
-        action += np.log(np.cosh(0.5 * x_s[i]))
+        potential_contribution += 1. * x_s[i] * x_s[i]
 
-    # Now contribution from the interaction terms.
-    potential = 0.
-    for i in range(N_1):
-        for j in range(i + 1, N_1):
-            potential -= np.log(abs(np.tanh(gaussian_weight * (x_s[i] - x_s[j]))))
+    potential_contribution *= 0.5
 
-    for i in range(N_1 + 1, N):
-        for j in range(i + 1, N):
-            potential -= np.log(abs(np.tanh(gaussian_weight * (x_s[i] - x_s[j]))))
-
-    potential *= 2.
-    
-    action = action + potential
-
-    hamiltonian = kinetic_contribution + action
+    hamiltonian = kinetic_contribution + potential_contribution
 
     return hamiltonian
-
-
-def tests():
-    x_s = [0.5, 0.6]
-    forces = calculate_force(x_s, gaussian_weight=1.)
-
-    print "The force for x = ", x_s[0], " is ", forces
-
-
+    
 def hmc(n_step=1, x_s={}, p_s={}):
 
     # First, choose a position freely.
@@ -73,11 +52,8 @@ def hmc(n_step=1, x_s={}, p_s={}):
 
     for i in xrange(0, N - 1, 2):
         # Generate a random number in the range- [0.5, 0.5)
-        # x_s[i] = np.random.random() - 0.5  
-        # x_s[i + 1] = np.random.random() - 0.5
-
-        x_s[i] = 0.5  
-        x_s[i + 1] = 0.6
+        x_s[i] = np.random.random() - 0.5  
+        x_s[i + 1] = np.random.random() - 0.5
 
 
     # Next, generate canonical momentum.
@@ -86,16 +62,12 @@ def hmc(n_step=1, x_s={}, p_s={}):
 
     for i in xrange(0, N - 1, 2):
         # Scale by sqrt(pi) to make the coefficient 1.
-        # p_s[i] = np.sqrt(np.pi) * np.random.normal(loc=0.0, scale=sigma)
-        # p_s[i + 1] = np.sqrt(np.pi) * np.random.normal(loc=0.0, scale=sigma)
-        p_s[i] = 0.1
-        p_s[i + 1] = 0.2
+        p_s[i] = np.sqrt(np.pi) * np.random.normal(loc=0.0, scale=sigma)
+        p_s[i + 1] = np.sqrt(np.pi) * np.random.normal(loc=0.0, scale=sigma)
 
 
     all_x_s, all_p_s = [], []
-    
-    ratio_values = []
-
+ 
     while n_step < total_steps:
 
         # print "Step #{}".format(n_step)
@@ -107,18 +79,9 @@ def hmc(n_step=1, x_s={}, p_s={}):
         # So we need to find the force.
         forces = calculate_force(x_s)
 
-
-
-        
         # Now, use the leapfrog method to propagate the system.
-        # p_2_s = {}
-        # for i in range(N):
-        #     p_2_s[i] = p_s[i] + (delta_T / 2) * forces[i]
-
         p_2_s = {}              # p_2_s = p(t + delta_T / 2)
         for i in range(N):
-            print "The forces for x = ", x_s[i], "are", forces
-
             p_2_s[i] = p_s[i] - (delta_T / 2) * x_s[i]
 
         x_3_s = {}              # x_3_s = x(t + delta_T)
@@ -154,13 +117,11 @@ def hmc(n_step=1, x_s={}, p_s={}):
             new_p_s = p_s
 
         # Calculate the raito of free energy.
-        ratio_value = 1.
+        ratio_value = 0.
         for i in range(N_1):
             for j in range(N_1, N):
-                ratio_value *= np.tanh(new_x_s[i] - new_x_s[j]) ** 2
-        
-
-        ratio_values.append( ratio_value )
+                ratio_value = np.tanh(new_x_s[i] - new_x_s[j]) ** 2
+                print ratio_value
 
         # hmc(n_step=(n_step + 1), x_s=new_x_s, p_s=new_p_s)
         x_s, p_s = new_x_s, new_p_s
@@ -170,12 +131,37 @@ def hmc(n_step=1, x_s={}, p_s={}):
         n_step += 1
 
 
-    # print ratio_values
-    # print [i for i in range(len(ratio_values))]
-    # plt.plot([i for i in range(len(ratio_values))], ratio_values)
 
-    # plt.show()
+    x_to_plot = []
+    p_to_plot = []
 
+    x_2_to_plot = []
+    p_2_to_plot = []
+
+    for i in range(len(all_x_s)):
+        x_to_plot.append( all_x_s[i][0] )
+        p_to_plot.append( all_p_s[i][0] )
+
+        x_2_to_plot.append( all_x_s[i][1] )
+        p_2_to_plot.append( all_p_s[i][1] )
+
+
+    
+
+    # plt.hist(x_to_plot,   bins=25, color='red', lw=3, histtype='step', label="x_1")
+    # plt.hist(x_2_to_plot, bins=25, color='blue', lw=3, histtype='step', label="x_2")
+    # plt.hist(p_to_plot, bins=25, color='green', lw=3, histtype='step', label="p_1")
+    # plt.hist(p_2_to_plot, bins=25, color='orange', lw=3, histtype='step', label="p_2")
+
+    plt.plot(x_to_plot, p_to_plot, lw=0, marker="*", ms=10, color="red", label="0")
+    plt.plot(x_2_to_plot, p_2_to_plot, lw=0, marker="*", ms=10, color="blue", label="1")
+
+    plt.legend()
+
+    # plt.xlabel("x")
+    # plt.ylabel("p")
+
+    plt.show()
 
         
 
@@ -201,5 +187,3 @@ def weird_distribution():
 
 if __name__ == '__main__':
     hmc()
-
-    # tests()
